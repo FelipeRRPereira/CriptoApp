9e43ed0a4e25faa674b2a9183d00ad7def2242de
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-03-03 23:42:29 -0300

    Creating news list with scroll.

--- lib/views/coin_detail.dart
+++ lib/views/coin_detail.dart
@@ -39,46 +39,73 @@ class CoinDetail extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Card(
-                          color: Color.fromRGBO(245, 49, 127, 1),
-                          child: Container(
-                            margin: EdgeInsets.only(bottom: 30.0),
-                            width: constaints.maxWidth,
-                            child: Column(
-                              children: <Widget>[
-                                CoinCard(
-                                  icon: null,
-                                  name: 'BTC Bitcoin',
-                                  value: '\$6687.49',
-                                  percentage: '6.34',
-                                  color: Color.fromRGBO(245, 49, 127, 1),
-                                  elev: 0.0,
-                                ),
-                                Image(
-                                  image: AssetImage('images/ic_chart.png'),
-                                  width: 278,
-                                  height: 62,
-                                ),
-                              ],
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: Card(
+                            color: Color.fromRGBO(245, 49, 127, 1),
+                            child: Container(
+                              margin: EdgeInsets.only(bottom: 30.0),
+                              width: constaints.maxWidth,
+                              child: Column(
+                                children: <Widget>[
+                                  CoinCard(
+                                    icon: null,
+                                    name: 'BTC Bitcoin',
+                                    value: '\$6687.49',
+                                    percentage: '6.34',
+                                    color: Color.fromRGBO(245, 49, 127, 1),
+                                    elev: 0.0,
+                                  ),
+                                  Image(
+                                    image: AssetImage('images/ic_chart.png'),
+                                    width: 278,
+                                    height: 62,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
                         Card(
-                          color: Color.fromRGBO(245, 49, 127, 1),
                           child: Container(
-                            margin: EdgeInsets.only(bottom: 30.0),
                             width: constaints.maxWidth,
-                            child: Column(
-                              children: <Widget>[
-                                CoinCard(
-                                  icon: null,
-                                  name: 'BTC Bitcoin',
-                                  value: '\$6687.49',
-                                  percentage: '6.34',
-                                  color: Color.fromRGBO(245, 49, 127, 1),
-                                  elev: 0.0,
-                                ),
-                              ],
+                            padding: EdgeInsets.only(top: 15.0),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 10.0),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        Text(
+                                          'NEWS',
+                                          style: TextStyle(
+                                            color:
+                                                Color.fromRGBO(26, 26, 37, 0.5),
+                                            fontSize: 16.0,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 340,
+                                    child: ListView(
+                                      children: <Widget>[
+                                        News(),
+                                        News(),
+                                        News(),
+                                        News(),
+                                        News(),
+                                        News(),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         )
@@ -89,6 +116,49 @@ class CoinDetail extends StatelessWidget {
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class News extends StatelessWidget {
+  const News({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: 5.0, left: 0.0),
+      elevation: 0.0,
+      child: ListTile(
+        leading: Image(
+          image: AssetImage('images/person_chart.png'),
+        ),
+        title: Text(
+          'CME Report: Bitcoin Futures Average Daily Volume up 93% in Second Quarter',
+          style: TextStyle(
+            fontSize: 14.0,
+            color: Color.fromRGBO(74, 74, 74, 1),
+          ),
+        ),
+        subtitle: Row(
+          children: <Widget>[
+            Icon(
+              Icons.access_time,
+              color: Color.fromRGBO(125, 129, 136, 1),
+              size: 16.0,
+            ),
+            Text(
+              '14:30 pm 10 July 2020',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Color.fromRGBO(125, 129, 136, 1),
+              ),
+            ),
+          ],
         ),
       ),
     );
